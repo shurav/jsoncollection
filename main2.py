8e7734c5f2ce77e85b488ae9bdab47e1a6a4b63b
Shurav Nandy <nandyshurav@gmail.com>
2022-10-01 17:07:00 -0400

    new commit

---
+++ main2.py
@@ -0,0 +1,21 @@
+import json
+a = {
+        "people" : [
+            {
+                "name" : "john",
+                "age" : 23,
+                "height" : 82/12
+            },
+            {
+                "name" : 'eric',
+                "age" : 31,
+                "height" : 6
+            },
+            {
+                "name" : "will",
+                "age" : 25,
+                "height" : 5.75
+            }
+            ]
+        }
+print(json.dumps(a, indent = 3, sort_keys = False, separators = ("-- ", "== ")))
