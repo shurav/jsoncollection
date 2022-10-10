b10c8c64729ae2e18e622220aa8585132f9a9173
Shurav Nandy <nandyshurav@gmail.com>
2022-10-10 17:09:50 -0400

    included net worths as a separate key

--- main2.py
+++ main2.py
@@ -25,4 +25,25 @@ with open("people.json", "w") as f:
     f.write(json.dumps(a, indent = 3))
 with open("people.json", "r") as f:
     new = json.loads(f.read())
+f.close()
 print(new)
+new["networths"] = [
+            {
+                "name" : "john",
+                "nw" : 200*10**3
+            },
+            {
+                "name" : "eric",
+                "nw" : 60*10**3
+            },
+            {
+                "name" : "will",
+                "nw" : 90*10**3
+            }
+            ]
+with open("people.json", "w") as g:
+    g.write(json.dumps(new, indent = 3))
+with open("people.json", "r") as g:
+    includeNet = json.loads(g.read())
+g.close()
+print(includeNet)
